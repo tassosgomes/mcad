bd99313081f2dcc58b73f90677bd143fb60516b4
Tasso Gomes <tasso.gomes@outlook.com>
2026-05-14 23:04:53 -0300

    chore(authz): consumir SDKs publicados
    - Trocar ProjectReference local do Authz por pacotes NuGet nas APIs .NET
    
    - Remover copia de libs/dotnet dos Dockerfiles de cadastro e identificacao
    
    - Remover SDK .NET local do repositorio mcad apos publicacao

--- libs/dotnet/Ecad.Authz.Sdk/IEcadAuthzClient.cs
+++
@@ -1,9 +0,0 @@
-namespace Ecad.Authz.Sdk;
-
-public interface IEcadAuthzClient
-{
-    Task<AuthzDecision> CheckAsync(
-        AuthzCheckRequest request,
-        string? bearerToken,
-        CancellationToken cancellationToken = default);
-}
